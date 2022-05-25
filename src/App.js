11f747dbb1f97b8cfb0c2c03ac6367fabf7f9578
ouweiya <higogle1@gmail.com>
2022-05-25 12:13:07 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Button from '@mui/material/Button';
+// const LazyButton = React.lazy(() => import('@mui/material/Button'));
+
+const App = () => {
+  return (
+    <div>
+      <Button variant="contained">Contained</Button>
+      {/* <LazyButton></LazyButton> */}
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [new HtmlWebpackPlugin({ template: 'index.html' })],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset',
+      },
+    ],
+  },
+};
+
+module.exports = config;
